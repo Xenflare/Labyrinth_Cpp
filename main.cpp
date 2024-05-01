f3913ef6928e15c0de8e8ce51301024020af524c
Lumix <elijah.clark.holmes@gmail.com>
2024-05-01 09:17:14 -0500

    1st Push

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <any>
+using namespace std;
+int main() {
+
+    list<string> registeredCharacters;
+    unordered_map<string,int> inventory;
+    unordered_map<string,unordered_map<string,any>> enemyPresets;
+    return 0;
+}
