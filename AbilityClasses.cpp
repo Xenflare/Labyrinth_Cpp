813af9dbc586914107ff0e5f39c2a726cc88025a
NetherKing16 King <NetherKingAsh@gmail.com>
2024-05-17 12:33:07 +0000

    made the swing attack into a generic attack

--- AbilityClasses.cpp
+++ AbilityClasses.cpp
@@ -2,24 +2,25 @@
 #include "Enemies.cpp"
 #include "Player.cpp"
 
-class SwingAttack {
+class Attack {
     public:
         string Name;
         int DMG[2];
         int BleedChance;
         int BleedDuration;
         int RequiredEnergy;
-        bool Global = false;
+        bool Global;
 
-        SwingAttack(string name, int baseDamage[2], int bleedChance, int bleedDuration, int energyUse) {
+        Attack(string name, int baseDamage[2], int bleedChance, int bleedDuration, int energyUse, bool isGlobal) {
             Name = name;
             DMG[2] = baseDamage[2];
             BleedChance = bleedChance;
             BleedDuration = bleedDuration;
             RequiredEnergy = energyUse;
+            Global = isGlobal;
         }
 
-        void Trigger(EnemyInstance& target, SwingAttack&) {
+        void Trigger(EnemyInstance& target, Attack&) {
             int damageToDeal = RandomNumber(DMG[0],DMG[1]);
 
             if (typeid(target) == typeid(EnemyInstance)) {
